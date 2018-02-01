638132319137eabc06d0fcf980992bb02e1b302b
李凤焕 <huanent@hotmail.com>
2018-02-01 09:48:32 +0800

    小修正

--- AnyTask/LoopExtensions.cs
+++ AnyTask/LoopExtensions.cs
@@ -30,7 +30,7 @@ namespace AnyTask
                         try
                         {
 #if NET40
-                            Task.WaitAll(new Task(() =>
+                            Task.WaitAll(Task.Factory.StartNew(() =>
                             {
                                 Thread.Sleep(interval.Value);
                             }, job._cancellationTokenSource.Token));
